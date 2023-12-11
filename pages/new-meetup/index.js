66f79dfb2590a94d6087da95ba7dd5f8c8718dc3
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-11 19:22:33 +0330

    Adding head Metadata

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import Head from "next/head";
 function NewMeetupPage() {
   const router = useRouter();
   async function addMeetupHandler(enteredMeetupData) {
@@ -15,6 +16,17 @@ function NewMeetupPage() {
     console.log(data);
     router.push("/");
   }
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  );
 }
 export default NewMeetupPage;
